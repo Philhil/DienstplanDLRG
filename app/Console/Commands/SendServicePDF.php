ed3f96943f9e646b5c80e898c2b4250b91fb6d30
Flip <philippe@pilarkto.net>
2017-09-09 12:32:31 +0200

    travis test

---
+++ app/Console/Commands/SendServicePDF.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\PositionAssigned;
+use App\Position;
+use App\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class SendServicePDF extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dlrg:SendServicePDF';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends Service Sheldue in PDF Format';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        echo "Test";
+
+    }
+}
