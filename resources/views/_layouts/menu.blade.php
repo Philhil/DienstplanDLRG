e9de197d5ed6d93cccd13cca1ec2cdc72eca7ca4
Flip <philippe@pilarkto.net>
2017-09-09 23:21:59 +0200

    remove profile img

--- resources/views/_layouts/menu.blade.php
+++ resources/views/_layouts/menu.blade.php
@@ -2,9 +2,6 @@
 <aside id="leftsidebar" class="sidebar">
     <!-- User Info -->
     <div class="user-info">
-        <div class="image">
-            <img src="../../images/user.png" width="48" height="48" alt="User" />
-        </div>
         <div class="info-container">
             <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{Auth::user()->first_name}} {{Auth::user()->name}}</div>
             <div class="email">{{Auth::user()->email}}</div>
