5eb1eefb4b970a4b5b312c66c567cb0ca246ce0e
Flip <philippe@pilarkto.net>
2017-09-10 22:40:29 +0200

    Events and Email Notification #2 SendServices cron every Monday at 7:00

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,8 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('dlrg:SendServicePDF')->weekly()->mondays()->at('07:00');
     }
 
     /**
