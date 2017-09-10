a4f2974b08e0feb47cbececb11053b31ef8ab60c
Flip <philippe@pilarkto.net>
2017-09-11 01:41:03 +0200

    add home link to header

--- resources/views/_layouts/header.blade.php
+++ resources/views/_layouts/header.blade.php
@@ -4,7 +4,7 @@
         <div class="navbar-header">
             <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
             <a href="javascript:void(0);" class="bars"></a>
-            <a class="navbar-brand" href="">DLRG Stuttgart</a>
+            <a class="navbar-brand" href="/home">DLRG Stuttgart</a>
         </div>
         <div class="collapse navbar-collapse" id="navbar-collapse">
             <ul class="nav navbar-nav navbar-right">
