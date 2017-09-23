260c5ee007566722623b52bf1bb6d3b2d428bb33
Flip <philippe@pilarkto.net>
2017-09-23 16:23:16 +0200

    improve ui

--- resources/views/news/_form.blade.php
+++ resources/views/news/_form.blade.php
@@ -4,33 +4,30 @@
     {{ Form::model($news, ['action' => ['NewsController@store', 'id' => $news->id]]) }}
 @endif
 
-<div class="row clearfix">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
-        <div class="row clearfix">
-            <div class="col-sm-10">
-                <div class="form-group {{ $errors->has('comment') ? 'has-error' : ''}}">
-                    <div class="form-line">
-                        {{ Form::label('title', 'Titel:') }}
-                        {{ Form::text('title', old('title'), ['placeholder' => "Titel...", 'class' => 'form-control no-resize']) }}
-                        {!! $errors->first('title', '<p class="help-block">:message</p>') !!}
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- TinyMCE -->
-        <div class="row clearfix">
-            <div class="form-group {{ $errors->has('content') ? 'has-error' : ''}}">
+    <div class="row clearfix">
+        <div class="col-sm-10">
+            <div class="form-group {{ $errors->has('comment') ? 'has-error' : ''}}">
                 <div class="form-line">
-                    {{ Form::textarea('content', old('content'), ['class' => 'form-control', 'id'=>"tinymce"]) }}
-                    {!! $errors->first('content', '<p class="help-block">:message</p>') !!}
+                    {{ Form::label('title', 'Titel:') }}
+                    {{ Form::text('title', old('title'), ['placeholder' => "Titel...", 'class' => 'form-control no-resize']) }}
+                    {!! $errors->first('title', '<p class="help-block">:message</p>') !!}
                 </div>
             </div>
         </div>
-        <!-- #END# TinyMCE -->
     </div>
 
+    <!-- TinyMCE -->
+    <div class="row clearfix">
+        <div class="form-group {{ $errors->has('content') ? 'has-error' : ''}}">
+            <div class="form-line">
+                {{ Form::textarea('content', old('content'), ['class' => 'form-control', 'id'=>"tinymce"]) }}
+                {!! $errors->first('content', '<p class="help-block">:message</p>') !!}
+            </div>
+        </div>
+    </div>
+    <!-- #END# TinyMCE -->
 </div>
 
 <div class="row clearfix">
--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -8,19 +8,17 @@
 @section('content')
     <div class="container-fluid">
         <!-- Input -->
-        <div class="row clearfix">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="card">
-                    <div class="header">
-                        <h2>
-                            {{$user->first_name}} {{$user->name}}
-                        </h2>
-                    </div>
-                    <div class="body">
-                        <div class="row clearfix">
-                            <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
-                                @include('user._form')
-                            </div>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="card">
+                <div class="header">
+                    <h2>
+                        {{$user->first_name}} {{$user->name}}
+                    </h2>
+                </div>
+                <div class="body">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                            @include('user._form')
                         </div>
                     </div>
                 </div>
