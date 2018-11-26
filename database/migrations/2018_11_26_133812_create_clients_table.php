02024ed2a33355c33db68e844c6c9b43afeb60af
Philippe Kaeufer <philippe.kaeufer@altran.com>
2018-11-26 15:57:46 +0100

    add clients table

---
+++ database/migrations/2018_11_26_133812_create_clients_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->date('seasonStart')->default("2000-01-01");
+            $table->boolean('isMailinglistCommunication')->default(false);
+            $table->string('mailinglistAddress')->nullable()->default(NULL);
+            $table->string('mailSenderName');
+            $table->string('mailSenderAddress');
+            $table->string('mailServer');
+            $table->string('mailPort');
+            $table->string('mailUser');
+            $table->string('mailPassword');
+            $table->time('defaultServiceStart')->default("10:00");
+            $table->time('defaultServiceEnd')->default("19:00");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
