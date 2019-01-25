cd846abbb62778019b3095f3b1779acd60a25764
Flip <philippe@pilarkto.net>
2019-01-25 13:55:08 +0100

    change Password via forgot password

--- resources/views/user/_form.blade.php
+++ resources/views/user/_form.blade.php
@@ -39,15 +39,7 @@
     </div>
 </div>
 
-<div class="col-sm-10">
-    <div class="form-group {{ $errors->has('password') ? 'has-error' : ''}}">
-        <div class="form-line">
-            {{ Form::label('password', 'Passwort:') }}
-            {{ Form::password('password', ['class' => 'form-control', 'placeholder' => 'Passwort']) }}
-            {!! $errors->first('password', '<p class="help-block">:message</p>') !!}
-        </div>
-    </div>
-</div>
+
 
 @if(\Illuminate\Support\Facades\Auth::user()->can('administration'))
 
