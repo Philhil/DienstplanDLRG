951acfb1d35de906c362ac34b7248f8859e9c18d
Flip <philippe@pilarkto.net>
2019-01-28 11:14:19 +0100

    multi-client application #37 news

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -65,7 +65,7 @@
 
                 <!-- NEWS -->
                 <div class="row clearfix">
-                    @foreach(\App\News::orderBy('created_at', 'DESC')->take(4)->get() as $news)
+                    @foreach(\App\News::where('client_id', '=', Auth::user()->currentclient_id)->orderBy('created_at', 'DESC')->take(1)->get() as $news)
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <div class="card">
                                 <div class="header bg-blue-grey">
