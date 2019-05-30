aecbe90b8be7bf840d199aee4f8c29114317a9e4
Flip <philippe@pilarkto.net>
2019-05-30 15:18:58 +0200

    add ics event to each approve email #18

--- app/Mail/PositionAssigned.php
+++ app/Mail/PositionAssigned.php
@@ -38,35 +38,23 @@ class PositionAssigned extends Mailable implements ShouldQueue
         $client = $this->position->service()->first()->client()->first();
         $service = $this->position->service()->first();
 
-        define('ICAL_FORMAT', 'Ymd');
 
-        $icalObject = "BEGIN:VCALENDAR
-                        VERSION:2.0
-                        METHOD:PUBLISH
-                        PRODID:-//DLRG//Dienstplan//DE\n";
+        $vCalendar = new \Eluceo\iCal\Component\Calendar('www.dlrgdienstplan.de');
+        $vEvent = new \Eluceo\iCal\Component\Event();
+        $vEvent
+            ->setDtStart(new \DateTime($service->toDateString()))
+            ->setDtEnd(new \DateTime($service->toDateString()))
+            ->setNoTime(true)
+            ->setSummary('DLRG Dienst' . $service->comment);
 
-            $icalObject .=
-                "BEGIN:VEVENT
-                DTSTART:" . date(ICAL_FORMAT, strtotime($service->toDateString())) . "
-                DTEND:" . date(ICAL_FORMAT, strtotime($service->date->toDateString())) . "
-                DTSTAMP:" . date(ICAL_FORMAT, strtotime($service->created_at)) . "
-                SUMMARY: DLRG Dienst $service->comment
-                UID:$service->id
-                STATUS:CONFIRMED
-                LAST-MODIFIED:" . date(ICAL_FORMAT, strtotime($service->updated_at)) . "
-                LOCATION:DLRG
-                END:VEVENT\n";
-
-        // close calendar
-        $icalObject .= "END:VCALENDAR";
-        $icalObject = str_replace(' ', '', $icalObject);
+        $vCalendar->addComponent($vEvent);
 
         return $this->subject('DLRG DIENSTE: Dienst zugewiesen')->view('email.position')->with([
             'position' => $this->position,
             'servicepositions' => $this->servicepositions,
             'authorizedby' => $this->authorizedby,
         ])->from($client->mailReplyAddress, $client->mailSenderName)
-        ->attachData($icalObject, 'dienst.ics', [
+        ->attachData($vCalendar->render(), 'dienst.ics', [
             'mime' => 'text/calendar;charset=UTF-8;method=REQUEST',
         ]);
     }
