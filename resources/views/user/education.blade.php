6ef9c35df51995f1d2f22660446e4c334bc3ba5e
Nogravitiy <58252937+Nogravitiy@users.noreply.github.com>
2019-12-20 10:17:24 +0100

    Create education.blade.php

---
+++ resources/views/user/education.blade.php
@@ -0,0 +1,42 @@
+<div class="col-sm-10">
+    <div class="form-group">
+        <h5>Wasserrettungsdienst</h5>
+        <p>Fortbildungspunkte:</p>
+        <div class="number count-to" data-from="0" data-to="{{$wrd}}" data-speed="1000" data-fresh-interval="20">{{$wrd}} von x  </div>
+    </div>
+</div>
+<div class="col-sm-10">
+    <div class="form-group">
+        <h5>Boot</h5>
+        <p>Fortbildungspunkte:</p>
+        <div class="number count-to" data-from="0" data-to="{{$boot}}" data-speed="1000" data-fresh-interval="20">{{$boot}} von 3h </div>
+    </div>
+</div>
+<div class="col-sm-10">
+    <div class="form-group">
+        <h5>Strömungsrettung</h5>
+        <p>Fortbildungspunkte:</p>
+        <div class="number count-to" data-from="0" data-to="{{$sr}}" data-speed="1000" data-fresh-interval="20">{{$sr}} von x </div>
+    </div>
+</div>
+<div class="col-sm-10">
+    <div class="form-group">
+        <h5>Rettungsdienst/San</h5>
+        <p>Fortbildungspunkte:</p>
+        <div class="number count-to" data-from="0" data-to="{{$rd}}" data-speed="1000" data-fresh-interval="20">{{$rd}} von 8h (30h)</div>
+    </div>
+</div>
+<div class="col-sm-10">
+    <div class="form-group">
+        <h5>Führung</h5>
+        <p>Fortbildungspunkte:</p>
+        <div class="number count-to" data-from="0" data-to="{{$f}}" data-speed="1000" data-fresh-interval="20">{{$f}} von x</div>
+    </div>
+</div>
+<div class="col-sm-10">
+    <div class="form-group">
+        <h5>Run-Swim-Run</h5>
+        <p>Fortbildungspunkte:</p>
+        <div class="number count-to" data-from="0" data-to="{{$rsr}}" data-speed="1000" data-fresh-interval="20">{{$rsr}} von 1</div>
+    </div>
+</div>
