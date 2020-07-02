396e125ec38acf80548fbd98d1287cca3e6b29ec
Nogravitiy <58252937+Nogravitiy@users.noreply.github.com>
2020-07-02 16:12:14 +0200

    Update education.blade.php

--- resources/views/user/education.blade.php
+++ resources/views/user/education.blade.php
@@ -1,3 +1,4 @@
+@if(Auth::user()->hasQualification(3))
 <div class="col-sm-10">
     <div class="form-group">
         <h5>Wasserrettungsdienst</h5>
@@ -5,6 +6,9 @@
         <div class="number count-to" data-from="0" data-to="{{$wrd}}" data-speed="1000" data-fresh-interval="20">{{$wrd}} von x  </div>
     </div>
 </div>
+@endif
+
+@if(Auth::user()->hasQualification(5))
 <div class="col-sm-10">
     <div class="form-group">
         <h5>Boot</h5>
@@ -12,13 +16,19 @@
         <div class="number count-to" data-from="0" data-to="{{$boot}}" data-speed="1000" data-fresh-interval="20">{{$boot}} von 3h </div>
     </div>
 </div>
+@endif
+
+@if(Auth::user()->hasQualification(8))
 <div class="col-sm-10">
     <div class="form-group">
         <h5>Strömungsrettung</h5>
         <p>Fortbildungspunkte:</p>
-        <div class="number count-to" data-from="0" data-to="{{$sr}}" data-speed="1000" data-fresh-interval="20">{{$sr}} von x </div>
+        <div class="number count-to" data-from="0" data-to="{{$sr}}" data-speed="1000" data-fresh-interval="20">{{$sr}} von 1 </div>
     </div>
 </div>
+@endif
+
+@if(Auth::user()->hasQualification(4))
 <div class="col-sm-10">
     <div class="form-group">
         <h5>Rettungsdienst/San</h5>
@@ -26,6 +36,9 @@
         <div class="number count-to" data-from="0" data-to="{{$rd}}" data-speed="1000" data-fresh-interval="20">{{$rd}} von 8h (30h)</div>
     </div>
 </div>
+@endif
+
+@if(Auth::user()->hasQualification(1))
 <div class="col-sm-10">
     <div class="form-group">
         <h5>Führung</h5>
@@ -33,6 +46,9 @@
         <div class="number count-to" data-from="0" data-to="{{$f}}" data-speed="1000" data-fresh-interval="20">{{$f}} von x</div>
     </div>
 </div>
+@endif
+
+
 <div class="col-sm-10">
     <div class="form-group">
         <h5>Run-Swim-Run</h5>
