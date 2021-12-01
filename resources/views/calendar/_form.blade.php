b189c4dd9ae126efdd5d4333a8b8b4eb06b752a7
Nogravitiy <58252937+Nogravitiy@users.noreply.github.com>
2021-12-01 16:38:08 +0100

    Add files via upload

---
+++ resources/views/calendar/_form.blade.php
@@ -0,0 +1,161 @@
+@if(\Illuminate\Support\Facades\Route::current()->getName() == 'calendar.edit')
+    {{ Form::model($calendar->toArray(), ['action' => ['CalendarController@update', $calendar->id], 'method' => 'PUT']) }}
+@else
+    {{ Form::model($calendar->toArray(), ['action' => ['CalendarController@store', $calendar->id]]) }}
+@endif
+
+<div class="col-lg-5 col-md-12 col-sm-12 col-xs-12">
+    <div class="row clearfix">
+        <div class="col-sm-10">
+            <div class="form-group {{$errors->has('date') ? 'has-error' : ''}}">
+                <div class="form-line">
+                    {{Form::label('date', 'Von:')}}
+                    {{Form::text('date', old('date', !empty($calendar->date) ? $calendar->date
+                        ->format('d m Y H:i') : ''), ['id' => 'date-start', 'class' => 'date-start form-control', 'placeholder'=>'Datum auswählen...', 'required'=>"true"])}}
+                    {!! $errors->first('date', '<p class="help-block">:message</p>') !!}
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row clearfix">
+        <div class="col-sm-10">
+            <div class="form-group {{ $errors->has('dateEnd') ? 'has-error' : '' }}">
+                <div class="form-line">
+                    {{ Form::label('dateEnd', 'Bis:') }}
+                    {{ Form::text('dateEnd', old('dateEnd', !empty($calendar->dateEnd) ? $calendar->dateEnd->format('d m Y H:i') : ''), ['id' => 'date-end', 'class' => 'date-end form-control', 'placeholder'=>'Datum auswählen...']) }}
+                    {!! $errors->first('dateEnd', '<p class="help-block">:message</p>') !!}
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row clearfix">
+        <div class="col-sm-10">
+            <div class="form-group {{ $errors->has('title') ? 'has-error' : ''}}">
+                <div class="form-line">
+                    {{ Form::label('title', 'Titel:') }}
+                    {{ Form::text('title', old('title'), ['placeholder' => "Titel...", 'class' => 'form-control no-resize', 'required'=>"true"]) }}
+                    {!! $errors->first('title', '<p class="help-block">:message</p>') !!}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- TinyMCE -->
+    <div class="row clearfix">
+        <div class="col-sm-10">
+            <div class="form-group {{ $errors->has('verantwortlicher') ? 'has-error' : ''}}">
+                <div class="form-line">
+                    {{ Form::label('verantwortlicher', 'Verantwortlicher:') }}
+                    {{ Form::text('verantwortlicher', old('verantwortlicher'), ['placeholder' => "Verantwortlicher...", 'class' => 'form-control no-resize', 'required'=>"true"]) }}
+                    {!! $errors->first('verantwortlicher', '<p class="help-block">:message</p>') !!}
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- #END# TinyMCE -->
+
+    <div class="row clearfix">
+        <div class="col-sm-10">
+            <div class="form-group {{ $errors->has('location') ? 'has-error' : ''}}">
+                <div class="form-line">
+                    {{ Form::label('location', 'Ort (Mit Koordinaten darstellbar auf Karte):') }}
+                    {{ Form::text('location', old('location', $calendar->location), ['id' => 'location', 'class' => 'location form-control', 'placeholder'=>'Ort eingeben...']) }}
+                    {!! $errors->first('location', '<p class="help-block">:message</p>') !!}
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row clearfix">
+        <div class="col-sm-10">
+            <div class="form-group {{ $errors->has('sendbydatetime') ? 'has-error' : ''}}">
+                <div class="form-line">
+                    {{ Form::label('sendbydatetime', 'Automatisch E-Mail versenden am (Leer = deaktiviert):') }}
+                    {{ Form::text('sendbydatetime', old('sendbydatetime', !empty($calendar->sendbydatetime) ? $calendar->sendbydatetime->format('d m Y H:i') : ''), ['id' => 'sendbydatetime', 'class' => 'sendbydatetime form-control', 'placeholder'=>'Datum auswählen...']) }}
+                    {!! $errors->first('sendbydatetime', '<p class="help-block">:message</p>') !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-7 col-md-12 col-sm-12 col-xs-12">
+</div>
+
+<div class="row clearfix">
+    <div class="col-sm-1 pull-right">
+        <div class="form-line">
+            {{ Form::button('Speichern', ['class' => 'form-control btn btn-success waves-effect', 'type' => "submit"]) }}
+        </div>
+    </div>
+</div>
+
+{{ Form::close() }}
+
+
+@section('post_body')
+    <!-- Moment Plugin Js -->
+    <script src="/plugins/momentjs/moment.js"></script>
+
+    <!-- Bootstrap Material Datetime Picker Plugin Js -->
+    <script src="/plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="/plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+    <!-- TinyMCE -->
+    <script src="/plugins/tinymce/tinymce.js"></script>
+
+    <script>
+        $( document ).ready(function() {
+
+            $('#date-end').bootstrapMaterialDatePicker({
+                format: 'DD MM YYYY HH:mm',
+                clearButton: true,
+                weekStart: 1,
+                lang : 'de',
+                minDate : new Date()
+            });
+            $('#sendbydatetime').bootstrapMaterialDatePicker({
+                format: 'DD MM YYYY HH:mm',
+                clearButton: true,
+                weekStart: 1,
+                lang : 'de',
+                minDate : new Date()
+            });
+            $('#date-start').bootstrapMaterialDatePicker({
+                format: 'DD MM YYYY HH:mm',
+                clearButton: true,
+                weekStart: 1,
+                lang : 'de',
+                minDate : new Date()
+            }).on('change', function(e, date)
+            {
+                $('#date-end').bootstrapMaterialDatePicker('setMinDate', date);
+                $('#date-end').bootstrapMaterialDatePicker('setDate', moment(date).clone().add(3, 'hours'));
+
+                $('#sendbydatetime').bootstrapMaterialDatePicker('setMaxDate', date);
+            });
+
+            //TinyMCE
+            tinymce.init({
+                selector: "textarea#tinymce",
+                theme: "modern",
+                height: 300,
+                plugins: [
+                    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                    'searchreplace wordcount visualblocks visualchars code fullscreen',
+                    'insertdatetime media nonbreaking save table contextmenu directionality',
+                    'emoticons template paste textcolor colorpicker textpattern imagetools'
+                ],
+                toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+                toolbar2: 'print preview media | forecolor backcolor emoticons',
+                image_advtab: true
+            });
+            tinymce.suffix = ".min";
+            tinyMCE.baseURL = '/plugins/tinymce';
+
+
+
+        });
+    </script>
+@endsection
---
+++ resources/views/calendar/create.blade.php
@@ -0,0 +1,29 @@
+@extends('_layouts.application')
+
+@section('head')
+    <!-- Bootstrap Select Css -->
+    <link href="/plugins/bootstrap-select/css/bootstrap-select.css" rel="stylesheet" />
+
+    <!-- Bootstrap Material Datetime Picker Css -->
+    <link href="/plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css" rel="stylesheet" />
+@endsection
+
+@section('content')
+
+    <div class="container-fluid">
+        <!-- Input -->
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="card">
+                <div class="header bg-blue-grey">
+                    <h2>
+                        Aus-/Fortbildung anlegen (Kalendar)
+                    </h2>
+                </div>
+                <div class="body">
+                    @include('calendar._form')
+                </div>
+            </div>
+        </div>
+        <!-- End Input -->
+    </div>
+@endsection
