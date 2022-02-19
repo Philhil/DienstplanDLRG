79515c6f9981ddd3febd07ee6e27c0a4f16c504f
Flip <philippe@pilarkto.net>
2022-02-19 18:48:53 +0100

    improve holiday view

--- resources/views/holiday/edit.blade.php
+++ resources/views/holiday/edit.blade.php
@@ -4,22 +4,16 @@
 @endsection
 
 @section('content')
-    <div class="container-fluid">
-        <!-- Input -->
-        <div class="row clearfix">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="card">
-                    <div class="header">
-                        <h2>
-                            Abwesenheit bearbeiten
-                        </h2>
-                    </div>
-                    <div class="body">
-                        @include('holiday._form')
-                    </div>
-                </div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>
+                    Abwesenheit bearbeiten
+                </h2>
+            </div>
+            <div class="body">
+                @include('holiday._form')
             </div>
         </div>
-        <!-- #END# Input -->
     </div>
 @endsection
