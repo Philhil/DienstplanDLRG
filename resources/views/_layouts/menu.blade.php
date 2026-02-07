28e43823f8484ece9da90b72a05a3816f9870971
Philippe Kaeufer <philippe.kaeufer@vector.com>
2026-02-07 22:42:30 +0100

    bump version to 2026.1

--- resources/views/_layouts/menu.blade.php
+++ resources/views/_layouts/menu.blade.php
@@ -185,7 +185,7 @@
             Programming: &copy; 2017 - <?php echo date('Y') ?> <a href="https://github.com/Philhil/DienstplanDLRG">Philippe Käufer</a>.
         </div>
         <div class="version">
-            <b>Version: </b> 2025.1
+            <b>Version: </b> 2026.1
         </div>
     </div>
     <!-- #Footer -->
